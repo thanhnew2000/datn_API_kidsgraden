650531ea9af886108d6a361a2e674aae4f87a6bf
unknown <quanglxph07563@fpt.edu.vn>
2020-11-03 10:11:38 +0700

    'add-phan-hoi-don-thuoc'

--- app/Http/Controllers/PhanHoiDonThuoc/PhanHoiDonThuocController.php
+++ app/Http/Controllers/PhanHoiDonThuoc/PhanHoiDonThuocController.php
@@ -27,13 +27,15 @@ class PhanHoiDonThuocController extends Controller
     public function store(Request $request){
         $data =  $request->all();
         $data['type'] = 1;
+        // return $data;
         $this->PhanHoiDonThuocRepository->create($data);
         $id_hs = $data['nguoi_phan_hoi_id'];
         $hoc_sinh = $this->HocSinhRepository->find($id_hs);
+        // return $hoc_sinh->ten;
         $get_giao_vien = $hoc_sinh->getLop->GiaoVien()->select('user_id')->get();
 
         foreach ($get_giao_vien as $key => $data_giao_vien) {
-            $thongbao['title'] ='Thông báo đơn dặn thuốc';
+            $thongbao['title'] =$hoc_sinh->ten.' đã phản hồi về đơn dặn thuốc';
             $thongbao['content'] ='nội dung';
             $thongbao['route'] = 'route';
             $thongbao['user_id'] = $data_giao_vien->user_id;
